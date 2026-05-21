f844876c7ecb8276eff76cd08cf5ef25e198c1e5
turnerlk <lukasdsrdantas@gmail.com>
2026-05-21 14:53:07 -0300

    teste

--- src/router.tsx
+++ src/router.tsx
@@ -10,7 +10,7 @@ export const getRouter = () => {
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
-    basepath: '/dojo-xogum',
+    basepath: '/',
   });
 
   return router;
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  base: '/dojo-xogum/',
+  base: '/',
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
